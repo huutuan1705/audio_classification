148d48a2205b2c889ddf0a9b9b6a736e48e6eede
tuan.nh <tuan.nh@tinasoft.com.vn>
2024-12-06 10:28:56 +0700

    update training

--- training.py
+++ training.py
@@ -57,11 +57,11 @@ def training(model, train_dl, test_dl, num_epochs):
         
         model.train()
         for i, data in enumerate(tqdm(train_dl)):
-            inputs, labels = data[0].to(device), data[1].to(device)
+            inputs, labels = data[0].to(device, dtype=torch.float32), data[1].to(device, dtype=torch.long)
             
             # Normalize the inputs
             inputs_m, inputs_s = inputs.mean(), inputs.std()
-            inputs = (inputs - inputs_m) / inputs_s
+            inputs = (inputs - inputs_m) / (inputs_s + 1e-7)
             
             # Zero the parameter gradients
             optimizer.zero_grad()
@@ -70,7 +70,7 @@ def training(model, train_dl, test_dl, num_epochs):
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
-            # nn.utils.clip_grad_norm_(model.parameters(), 0.1)
+            nn.utils.clip_grad_norm_(model.parameters(), max_norm=0.1)
             optimizer.step()
             scheduler.step()
             
